d7ce6e2c2766610a361d64cbb89eb1a4df211ae3
chubaka-92 <chubaka_92@mail.ru>
2022-06-15 18:01:46 +0300

    start new project

---
+++ src/main/java/com/example/personmenegement/PersonMenegementApplication.java
@@ -0,0 +1,13 @@
+package com.example.personmenegement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonMenegementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonMenegementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/personmenegement/PersonMenegementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personmenegement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonMenegementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
