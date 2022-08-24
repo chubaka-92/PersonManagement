ff9e8e6dff5c9aa5d50ad6b419308c45d4005b45
chubaka-92 <chubaka_92@mail.ru>
2022-08-24 11:54:06 +0300

    fix todo part 2 for rest

--- src/main/java/com/example/personmenegement/services/MessageService.java
+++ src/main/java/com/example/personmenegement/services/MessageService.java
@@ -1,20 +1,22 @@
 package com.example.personmenegement.services;
 
-import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ResourceBundle;
 
-@AllArgsConstructor
+@Slf4j
 public class MessageService {// todo тут интерфейс лишний  //   DONE
 
     private static final String MESSAGE = "message";
     private final ResourceBundle resourceBundle = ResourceBundle.getBundle(MESSAGE);
 
     public String getMessage(String constant) {
+        log.debug("Was calling getMessage. Input constant: {}", constant);
         return resourceBundle.getString(constant);
     }
 
-    public String getMessage(Enum a) {
-        return getMessage(a.name());
+    public String getMessage(Enum constant) {
+        log.debug("Was calling getMessage. Input constant: {}", constant);
+        return getMessage(constant.name());
     }
 }
